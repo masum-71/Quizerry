e05ace48fc6c3f9ee28999e4c1d23434cf410ea3
Masum Billah <masum.monpura@gmail.com>
2022-10-10 23:38:17 +0600

    responsive navbar done

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar/NavBar';
+import './Main.css'
 
 const Main = () => {
     return (
-        <div>
+        <div className='main'>
             <NavBar></NavBar>
             <Outlet></Outlet>
         </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import "./NavBar.css";
 
 const NavBar = () => {
+
+  const [open, setOpen] = useState(false)
   return (
-    <nav>
-      <h1>Quizerry</h1>
-      <ul>
-        <Link to="/subjects">Subjects</Link>
-        <Link to="/statistics">Statistics</Link>
-        <Link to="/blog">Blog</Link>
+    <nav className="navbar">
+      <h1 className="logo text-4xl">Quizerry</h1>
+      <ul className={`md:flex md:static absolute duration-500 ease-in-out ${open ? 'top-7 right-0' : 'top-[-100px]'}`}>
+        <li><NavLink
+          className={({ isActive }) => (isActive ? "text-red-400" : undefined)}
+          to="/subjects"
+        >
+          Subjects
+        </NavLink></li>
+        <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-red-400" : undefined)}
+          to="/statistics"
+        >
+          Statistics
+        </NavLink>
+        </li>
+        <li>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-red-400" : undefined)}
+          to="/blog"
+        >
+          Blog
+        </NavLink>
+        </li>
       </ul>
+     <div onClick={() => setOpen(!open)} className="h-6 w-6 text-blue-500 md:hidden">
+     {
+        open ? <XMarkIcon ></XMarkIcon> : <Bars3Icon/>
+      }
+      
+     </div>
     </nav>
   );
 };
